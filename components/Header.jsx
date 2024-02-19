eb7ae9049d324377f5422d22f9e9687453c5c06a
ecpieritz <ecpieritz@gmail.com>
2024-02-19 10:44:09 -0300

    Add header

---
+++ components/Header.jsx
@@ -0,0 +1,48 @@
+import style from '@/styles/Header.module.css'
+
+export default function Header() {
+  return (
+    <header className={style.shHeader} id="sh-header">
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <a className={style.shBrand} href="/">
+          Shade.io
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#sh-header">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#sh-services">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#sh-testimonials">
+                Testimonials
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#sh-contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +13,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
       <main>
         <div className="container">
-          <h2 className="text-primary">Hello World</h2>
+          <Header />
         </div>
       </main>
     </>
