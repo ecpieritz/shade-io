4d69ab911b97dbf4692cce2823af04f45ce8298b
ecpieritz <ecpieritz@gmail.com>
2024-02-19 11:35:15 -0300

    Add Banner component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
-
+import Banner from "@/components/Banner";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -13,10 +13,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
+      <Header />
+
       <main>
         <div className="container">
-          <Header />
+          <Banner
+            title='Make your business powerful with Shade.'
+            description='With lots of unique blocks, you can easily build a page without coding. Build your next consultancy website within few minutes.'
+            btnDesc='Get A Free Quote'
+            imgUrl='/images/sh-banner__img.png'
+          />
         </div>
       </main>
     </>
