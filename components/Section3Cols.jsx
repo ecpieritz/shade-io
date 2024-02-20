d63015119dd6fc286c3fbee749d048172018eda8
ecpieritz <ecpieritz@gmail.com>
2024-02-20 14:14:04 -0300

    Change col to col-md

--- components/Section3Cols.jsx
+++ components/Section3Cols.jsx
@@ -1,9 +1,9 @@
 export default function Section3Cols(props) {
   return (
     <div className="row align-items-center">
-      <div className="col-4">{props.col1}</div>
-      <div className="col-4">{props.col2}</div>
-      <div className="col-4">{props.col3}</div>
+      <div className="col-md-4">{props.col1}</div>
+      <div className="col-md-4">{props.col2}</div>
+      <div className="col-md-4">{props.col3}</div>
     </div>
   );
 }
