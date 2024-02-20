3bc9efe2a9d16433eab6d3bd77390a17f00eaa42
ecpieritz <ecpieritz@gmail.com>
2024-02-20 13:15:14 -0300

    Add card

---
+++ components/Card.jsx
@@ -0,0 +1,18 @@
+export default function Card(props) {
+  return (
+    <>
+    <h4>{props.cardTitle}</h4>
+    <div class="card border-0" style={{width: '100%'}}>
+      <img src={props.imgUrl} class="card-img-top" alt="..." />
+      <div class="card-body px-0">
+        <p class="card-text">
+          {props.cardText}
+        </p>
+        <a href="#" class="btn shPurpleOutlineBtn">
+          Learn more <img src="./images/sh-icon__right-arrow.png" alt="right arrow" />
+        </a>
+      </div>
+    </div>
+    </>
+  );
+}
